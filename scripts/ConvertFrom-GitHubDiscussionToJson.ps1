4cfd569e29422340a2a3166f1f2f25c9e00bdbc6
Jeff Bolduan <jbolduan@umn.edu>
2024-11-13 13:37:02 -0600

    Added script to generate data.json and ran first time

---
+++ scripts/ConvertFrom-GitHubDiscussionToJson.ps1
@@ -0,0 +1,156 @@
+# Use https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens to create a PAT
+if (Get-Module -ListAvailable -Name PSGraphQL) {
+    Import-Module -Name GitHub
+}
+else {
+    throw "Could not find module GitHub installed.  You need to: Install-Module GitHub"
+}
+$url = "https://api.github.com/graphql"
+
+$TOKEN = Get-Content -Path C:\temp\mytoken.txt
+
+$discussionsQuery = '
+query {
+  repository(owner:"jbolduan", name:"trumpwont.com") {
+    discussions(first: 10, categoryId: "DIC_kwDONOWonc4CkNhJ", orderBy: {field:CREATED_AT, direction: ASC}{cursor}) {
+      nodes {
+        id
+        title
+        bodyHTML
+        url
+        labels(first: 10) {
+          nodes {
+            name
+          }
+        }
+      }
+      pageInfo {
+        endCursor
+        startCursor
+        hasNextPage
+        hasPreviousPage
+      }
+    }
+  }
+}'
+
+$requestHeaders = @{
+    Authorization = "bearer $TOKEN"
+}
+
+[System.Collections.ArrayList]$discussions = @()
+$cursor = ""
+do {
+    $DiscussionsQuery.Replace("{cursor}", $cursor)
+    $results = Invoke-GraphQLQuery -Query $DiscussionsQuery.Replace("{cursor}", $cursor) -Uri $url -Headers $requestHeaders
+
+    if ($null -ne $results.data.repository.discussions.pageInfo.endCursor -or $results.data.repository.discussions.pageInfo.endCursor -ne "") {
+        $cursor = ", after: `"$($results.data.repository.discussions.pageInfo.endCursor)`""
+    }
+
+    $discussions.Add($results)
+} until(-not $results.data.repository.discussions.pageInfo.hasNextPage)
+
+# Build Promises section of Json Object
+[System.Collections.ArrayList]$promises = @()
+foreach ($discussion in $discussions.data.repository.discussions.nodes) {
+    $parsedHtml = $discussion.bodyHTML | ConvertFrom-Html
+    $section = ""
+    $description = ""
+    $status = ""
+    $status_info = ""
+    $category = ""
+    $sources = @()
+    foreach ($line in $parsedHtml.ChildNodes.InnerText) {
+        if ($line -eq "Description") {
+            $section = "description"
+        }
+        elseif ($line -eq "What is the current status of this promise?") {
+            $section = "status"
+        }
+        elseif ($line -eq "Status Description") {
+            $section = "status_info"
+        }
+        elseif ($line -eq "Category") {
+            $section = "category"
+        }
+        elseif ($line -eq "References (URLs)") {
+            $section = "sources"
+        }
+        else {
+            if ($section -eq "description" -and $line -ne "Description" -and $line -notmatch "\n") {
+                $description += $line
+            }
+            elseif ($section -eq "status" -and $line -ne "What is the current status of this promise?" -and $line -notmatch "\n") {
+                $status += $line
+            }
+            elseif ($section -eq "status_info" -and $line -ne "Status Description" -and $line -notmatch "\n") {
+                $status_info += $line
+            }
+            elseif ($section -eq "category" -and $line -ne "Category" -and $line -notmatch "\n") {
+                $category += $line
+            }
+            elseif ($section -eq "sources" -and $line -ne "References (URLs)") {
+                if ($null -ne $line -and $line -ne "" -and $line -ne "`n") {
+                    $sources += $line.Split("`n")
+                }
+            }
+        }
+    }
+    $sources.ToArray().Count
+    $null = $promises.Add([PSCustomObject][ordered]@{
+            title       = $discussion.title.Substring(10)
+            description = $description
+            status      = $status
+            status_info = $status_info
+            category    = $category
+            comments    = $discussion.url
+            sources     = $sources.ToArray()
+        })
+}
+
+# Build Json
+[pscustomobject]$data = [ordered]@{
+    icons    = [ordered]@{
+        "First 24 Hours"     = "clock"
+        "First 100 Days"     = "calendar"
+        "Culture"            = "music"
+        "Economy"            = "dollar-sign"
+        "Environment"        = "leaf"
+        "Government"         = "university"
+        "Immigration"        = "suitcase"
+        "Indigenous"         = "users"
+        "Security"           = "fighter-jet"
+        "Health"             = "heart"
+        "World"              = "globe"
+        "Education"          = "graduation-cap"
+        "Energy"             = "lightbulb"
+        "Republican Promise" = "elephant"
+        "Influencer Promise" = "trash"
+    }
+    statuses = [ordered]@{
+        "Not started" = [ordered]@{
+            "color" = "info"
+            "icon"  = "hourglass-start"
+        }
+        "In progress" = [ordered]@{
+            "color" = "warning"
+            "icon"  = "cogs"
+        }
+        "Achieved"    = [ordered]@{
+            "color" = "success"
+            "icon"  = "check-circle"
+        }
+        "Broken"      = [ordered]@{
+            "color" = "danger"
+            "icon"  = "ban"
+        }
+        "Compromised" = [ordered]@{
+            "color" = "compromised"
+            "icon"  = "handshake"
+        }
+    }
+    promises = $promises.ToArray()
+}
+
+$data | convertto-json -Depth 10 -Compress | Set-Content -Path ".\_data\data.json"
